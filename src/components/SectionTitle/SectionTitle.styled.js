a6f1fe2417e2d2880afdaed33ce21ba4eef65969
Dhennis Nizal <dhnizal@gmail.com>
2025-11-27 13:23:43 +0800

    feat: add section title component

---
+++ src/components/SectionTitle/SectionTitle.styled.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import theme from "../../styles/Theme";
+
+export const SectionContainer = styled.div`
+  text-align: left;
+  margin-bottom: 2rem;
+`;
+
+export const Title = styled.h2`
+  font-size: 2rem;
+  font-weight: bold;
+  margin: 0;
+  color: ${(props) => props.titleColor || "${theme.color.black}"};
+  line-height: 1.2;
+`;
+
+export const SubTitle = styled.p`
+  font-size: 1rem;
+  margin: 0;
+  color: ${(props) => props.subtitleColor || "${theme.color.black}"};
+  line-height: 1.5;
+`;
