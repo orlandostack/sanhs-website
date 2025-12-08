ddefe833bf4874dc75485996060277bfde53ec4a
orly07 <delacruzorlando776@gmail.com>
2025-12-08 15:21:44 +0800

    refactor: hero cards

--- src/modules/Hero/Hero.jsx
+++ src/modules/Hero/Hero.jsx
@@ -1,13 +1,16 @@
 // src/modules/Hero/Hero.jsx
-import { memo } from "react";
+import { memo, useState } from "react";
 import { Link } from "react-scroll";
 import * as S from "./Hero.styled";
 import { heroData } from "../../data/heroData";
 import HeroButton from "../../components/Button/HeroButton/HeroButton";
 import HeroCard from "../../components/Cards/HeroCard/HeroCard";
 import theme from "../../styles/Theme";
+import HeroModal from "../../components/Modal/HeroModal";
 
 const Hero = memo(({ id }) => {
+  const [activeModal, setActiveModal] = useState(null);
+
   return (
     <S.HeroWrapper id={id}>
       <S.Overlay1 />
@@ -23,6 +26,7 @@ const Hero = memo(({ id }) => {
               {heroData.primaryButton}
             </HeroButton>
           </Link>
+
           <Link to="announcement" smooth duration={1000} spy={true}>
             <HeroButton $bgColor={theme.color.transparent}>
               {heroData.secondaryButton}
@@ -38,9 +42,17 @@ const Hero = memo(({ id }) => {
             logo={item.logo}
             title={item.title}
             subtitle={item.subtitle}
+            onClick={() => setActiveModal(item)}
           />
         ))}
       </S.CardContainer>
+
+      <HeroModal
+        open={!!activeModal}
+        onClose={() => setActiveModal(null)}
+        title={activeModal?.title}
+        content={activeModal?.modalContent}
+      />
     </S.HeroWrapper>
   );
 });
