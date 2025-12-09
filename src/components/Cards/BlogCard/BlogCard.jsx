95f884b8c3d762e59796859b593746295842516f
Dhennis Nizal <dhnizal@gmail.com>
2025-12-09 16:46:03 +0800

    feat: add blog content page

--- src/components/Cards/BlogCard/BlogCard.jsx
+++ src/components/Cards/BlogCard/BlogCard.jsx
@@ -1,4 +1,5 @@
 import { memo } from "react";
+import { useNavigate } from "react-router-dom";
 import { Collage } from "./BlogCard.styled";
 
 const formatDate = (dateString) => {
@@ -11,6 +12,12 @@ const formatDate = (dateString) => {
 };
 
 const BlogCard = memo(({ blogs }) => {
+  const navigate = useNavigate();
+
+  const handleNavigate = (id) => {
+    navigate(`/blog/${id}`);
+  };
+
   const sortedBlogs = [...blogs].sort(
     (a, b) => new Date(b.date) - new Date(a.date)
   );
@@ -26,7 +33,7 @@ const BlogCard = memo(({ blogs }) => {
   return (
     <Collage>
       <div className="LeftSide">
-        <div className="card">
+        <div className="card" onClick={() => handleNavigate(mostRecent.id)}>
           <img src={mostRecent.thumbnail} alt={mostRecent.title} />
           <div className="overlay">
             <div className="content">
@@ -39,8 +46,12 @@ const BlogCard = memo(({ blogs }) => {
       </div>
 
       <div className="RightSide">
-        {otherBlogs.map((blog, index) => (
-          <div key={blog.id} className="card">
+        {otherBlogs.map((blog) => (
+          <div
+            key={blog.id}
+            className="card"
+            onClick={() => handleNavigate(blog.id)}
+          >
             <img src={blog.thumbnail} alt={blog.title} />
             <div className="overlay">
               <div className="content">
---
+++ src/modules/BlogContent/BlogContent.styled.js
@@ -0,0 +1,85 @@
+import styled from "styled-components";
+import theme from "../../styles/Theme";
+
+export const Wrapper = styled.section`
+  height: auto;
+  width: 100%;
+  padding: 4rem 0;
+`;
+
+export const Content = styled.div`
+  max-width: 80vw;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+
+  ${theme.media.mobile} {
+    max-width: 90vw;
+  }
+`;
+
+export const IconWrapper = styled.div`
+  width: 3rem;
+  padding: 0.8rem;
+  margin-bottom: 2rem;
+  border-radius: 50%;
+  background-color: ${theme.color.gray[100]};
+  border: 1px solid ${theme.color.gray[300]};
+  cursor: pointer;
+
+  svg {
+    font-size: 1.3rem;
+  }
+
+  ${theme.media.mobile} {
+    width: 3rem;
+    padding: 0.8rem;
+
+    svg {
+      font-size: 1.3rem;
+    }
+  }
+`;
+
+export const ThumbnailImage = styled.img`
+  width: 100%;
+  max-height: 500px;
+  object-fit: cover;
+  border-radius: 8px;
+  margin-bottom: 1.5rem;
+`;
+
+export const Title = styled.h1`
+  font-size: 2.5rem;
+  color: ${theme.color.primaryAccent};
+  margin: 0;
+
+  ${theme.media.mobile} {
+    font-size: 2rem;
+  }
+`;
+
+export const Subtitle = styled.h2`
+  font-size: 1.5rem;
+  font-weight: 600;
+  line-height: 1.8rem;
+  color: ${theme.color.gray[700]};
+  margin: 0 0 2rem 0;
+
+  ${theme.media.mobile} {
+    font-size: 1.3rem;
+  }
+`;
+
+export const DateText = styled.h3`
+  font-size: 1rem;
+  font-style: italic;
+  color: ${theme.color.gray[500]};
+  margin: 0 0 2rem 0;
+`;
+
+export const BlogText = styled.p`
+  font-size: 1.1rem;
+  line-height: 1.8;
+  color: ${theme.color.gray[700]};
+`;
